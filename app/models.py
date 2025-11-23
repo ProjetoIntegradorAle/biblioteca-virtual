608c30f227bb6094fa2eb33516ad183b46099e19
luanatslima <luanatayna2007@gmail.com>
2025-11-23 11:58:57 -0300

    novo design em formulário

--- app/models.py
+++ app/models.py
@@ -20,7 +20,7 @@ class Material(models.Model):
     ]
     
     autor = models.CharField(max_length=100)
-    descricao = models.TextField(max_length=100)
+    descricao = models.TextField(max_length=200)
     titulo = models.CharField(max_length=200)
     tipo = models.CharField(
         max_length=2,
