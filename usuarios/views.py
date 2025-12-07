50c6cdd42b34283983e505029a8304fa6b2c9a59
luanatslima <luanatayna2007@gmail.com>
2025-12-07 11:45:48 -0300

    perfil do autor pode ser visualizado

--- usuarios/views.py
+++ usuarios/views.py
@@ -9,7 +9,8 @@ from django.contrib.auth import update_session_auth_hash, logout
 from django.contrib.auth.forms import PasswordChangeForm
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 def cadastro(request):
     if request.method == 'POST':
