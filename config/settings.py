28f24742ae0b92d69c3863a5c9d943968f1f798d
luanatslima <luanatayna2007@gmail.com>
2025-12-13 09:01:27 -0300

    Adicionando arquivos estáticos coletados

--- config/settings.py
+++ config/settings.py
@@ -133,6 +133,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
